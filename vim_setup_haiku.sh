3befa03acb69fede499c2ed964f107ab238e6d91
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-20 13:46:41 -0400

    Update vim_setup_haiku.sh

--- vim_setup_haiku.sh
+++ vim_setup_haiku.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# This script will setup Vim with the Catppuccin Mocha colorscheme, along with the Lightline plugin.
+# This script sets up Vim with the Catppuccino Mocha colorscheme and the Lightline plugin.
 
-# In this section, we will configure the vimrc file.
-echo "set number" >> /boot/home/config/settings/vim/vimrc
-echo "set cursorline" >> /boot/home/config/settings/vim/vimrc
-echo "set smoothscroll" >> /boot/home/config/settings/vim/vimrc
-echo "set termguicolors" >> /boot/home/config/settings/vim/vimrc
-echo "" >> /boot/home/config/settings/vim/vimrc
-echo "call plug#begin('~/config/settings/vim/plugged')" >> /boot/home/config/settings/vim/vimrc
-echo "Plug 'itchyny/lightline.vim'" >> /boot/home/config/settings/vim/vimrc
-echo "Plug 'catppuccin/vim', { 'as': 'catppuccin' }" >> /boot/home/config/settings/vim/vimrc
-echo "call plug#end()" >> /boot/home/config/settings/vim/vimrc
-echo "" >> /boot/home/config/settings/vim/vimrc
-echo "let g:lightline = {'colorscheme': 'catppuccin_mocha'}" >> /boot/home/config/settings/vim/vimrc
-echo "colorscheme catppuccin_mocha" >> /boot/home/config/settings/vim/vimrc
-echo "set laststatus=2" >> /boot/home/config/settings/vim/vimrc
-echo "set noshowmode" >> /boot/home/config/settings/vim/vimrc
+# Configure the vimrc file
+vimrc_path="/boot/home/config/settings/vim/vimrc"
+cat << EOF >> "$vimrc_path"
+set number
+set cursorline
+set smoothscroll
+set termguicolors
 
-# Get vim-plug.
-mkdir -p ~/config/settings/vim/autoload ; curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o ~/config/settings/vim/autoload/plug.vim
+call plug#begin('~/config/settings/vim/plugged')
+Plug 'itchyny/lightline.vim'
+Plug 'catppuccin/vim', { 'as': 'catppuccin' }
+call plug#end()
 
-# Install Catppuccin colors plugin.
-vim -es -u ~/config/settings/vim/vimrc -i NONE -c "PlugInstall" -c "qa"
+let g:lightline = {'colorscheme': 'catppuccin_mocha'}
+colorscheme catppuccin_mocha
+set laststatus=2
+set noshowmode
+EOF
+
+# Get vim-plug and install Catppuccin colors plugin.
+mkdir -p ~/config/settings/vim/autoload && curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o ~/config/settings/vim/autoload/plug.vim && vim -es -u "$vimrc_path" -i NONE -c "PlugInstall" -c "qa"
